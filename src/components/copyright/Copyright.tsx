bf7704eda1519c8ba832749a5c15d9355c76aaa0
Jarrett Huang <jarrett@jhuang.ca>
2023-07-07 23:31:38 -0700

    Update CopyRight component
    Add github icon and redirection to github repo.

--- src/components/copyright/Copyright.tsx
+++ src/components/copyright/Copyright.tsx
@@ -1,11 +1,24 @@
 import "./assets/css/Copyright.css";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 function Copyright() {
   const currentYear = new Date().getFullYear();
+
+  function handleGithubClick(): void {
+    window.location.assign("https://github.com/jarretthuang/json-viewer");
+  }
+
   return (
     <div className="Copyright">
-      <div className="copyright-banner">
-        <span>Copyright © {currentYear} Jarrett Huang</span>
+      <div className="copyright-banner flex flex-row items-center uppercase font-medium opacity-80">
+        <span>
+          <span className="copyright-header">Copyright</span> © {currentYear}{" "}
+          Jarrett Huang
+        </span>
+        <GitHubIcon
+          className="github-icon hover:opacity-80"
+          onClick={handleGithubClick}
+        />
       </div>
     </div>
   );
