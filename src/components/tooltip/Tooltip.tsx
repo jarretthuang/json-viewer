556b842cb599824c1570ba55929fc0841c73b77b
Jarrett Huang <jarrett@jhuang.ca>
2023-07-07 23:52:37 -0700

    Update Tooltip.tsx
    Allow overriding font size in Tooltip component.

--- src/components/tooltip/Tooltip.tsx
+++ src/components/tooltip/Tooltip.tsx
@@ -2,17 +2,23 @@ import { styled } from "@mui/material";
 import { default as MuiTooltip } from "@mui/material/Tooltip";
 import { TooltipProps, tooltipClasses } from "@mui/material/Tooltip";
 
-export const Tooltip = styled(({ className, ...props }: TooltipProps) => (
-  <MuiTooltip
-    {...props}
-    arrow
-    placement={props.placement ?? "right"}
-    classes={{ popper: className }}
-  />
-))(({ theme }) => ({
+type AdditionalProps = {
+  fontSize?: string;
+};
+
+export const Tooltip = styled(
+  ({ className, fontSize, ...props }: TooltipProps & AdditionalProps) => (
+    <MuiTooltip
+      {...props}
+      arrow
+      placement={props.placement ?? "right"}
+      classes={{ popper: className }}
+    />
+  )
+)<AdditionalProps>(({ theme, fontSize }) => ({
   [`& .${tooltipClasses.tooltip}`]: {
     boxShadow: theme.shadows[1],
-    fontSize: "0.4rem",
+    fontSize: fontSize ?? "0.4rem",
     userSelect: "none",
   },
 }));
