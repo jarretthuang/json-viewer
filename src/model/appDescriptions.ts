181d4caff440019741acb3998fffef6f997a3337
Jarrett Huang <jarethuang@gmail.com>
2023-11-05 01:41:02 -0700

    Update appDescriptions.ts

--- src/model/appDescriptions.ts
+++ src/model/appDescriptions.ts
@@ -1,7 +1,7 @@
 export const jsonViewerAppDescription = `
-The _JSON Viewer_ app is a helpful tool that makes it incredibly easy to work with JSON.
+Introducing the __JSON Viewer__ app, your indispensable tool for effortlessly managing JSON data.
 
-You can start off by pasting a block of text in the _Edit_ tab. There are options available for you to quickly perform certain actions such as format (i.e. auto-indent) or minimize.
+With the __Edit__ tab, you can start by pasting your JSON text. It offers a suite of convenient options for you to perform essential actions, such as auto-indenting and minimizing the JSON structure, making it easier to work with complex data.
 
-In the _View_ tab, a tree structure will be generated to help you visualize the JSON object in the UI.
+In the __View__ tab, the app creates a clear tree structure to provide you with an intuitive and visual representation of your JSON object. Simplify your JSON workflows with a user-friendly interface.
 `;
