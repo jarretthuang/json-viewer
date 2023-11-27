d5de582ec259c871c4edecc8b803012741efe0ab
Jarrett Huang <jarrett@jhuang.ca>
2023-11-26 21:12:31 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import NavBar from "@/components/nav-bar/NavBar";
 
 export default function App() {
   return (
-    <main className="flex min-h-screen flex-col justify-center">
+    <main className="flex h-full flex-col justify-center">
       <NavBar />
       <JsonViewer></JsonViewer>
     </main>
