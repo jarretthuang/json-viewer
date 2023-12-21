7a81b126bf0d671e13573a560a3d97f45c6d46ba
Jarrett Huang <jarrett@jhuang.ca>
2023-12-20 21:40:55 -0800

    Try adding icons manually in the html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,27 @@ export default function RootLayout({
         content="#0a0a0a"
       />
       <meta name="theme-color" content="#fdfeff" />
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#708cb4" />
+      </head>
       <body className={fontsCssClass}>
         {children}
         <Analytics />
