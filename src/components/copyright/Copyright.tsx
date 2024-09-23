a6942597cf4337d3ae05f3f8503898a01a9a1025
Jarrett Huang <jarrett@jhuang.ca>
2024-09-22 18:44:11 -0700

    Simplified copyright text.

--- src/components/copyright/Copyright.tsx
+++ src/components/copyright/Copyright.tsx
@@ -3,33 +3,47 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import LocalCafeIcon from "@mui/icons-material/LocalCafe";
 
 function Copyright() {
-  const currentYear = new Date().getFullYear();
-
   return (
-    <div className="Copyright">
-      <div className="copyright-banner flex flex-row items-center bg-white/20 font-medium uppercase shadow-subtle dark:shadow-subtleWhite">
-        <span>
-          <span className="copyright-header">Copyright</span> © {currentYear}{" "}
-          <span className="hidden md:inline">Jarrett Huang</span>
-          <span className="inline md:hidden">JH</span>
-        </span>
-        <div className="flex gap-1 pb-0.5 pl-0.5 pr-1 md:pb-0 md:pl-0">
-          <a href="https://github.com/jarretthuang/json-viewer" target="_blank">
-            <GitHubIcon
-              className="copyright-icon ml-1 w-[20px] hover:opacity-70 md:w-2"
-              fontSize="small"
-            />
-          </a>
-
-          <a href="https://www.buymeacoffee.com/jarretthuang" target="_blank">
-            <LocalCafeIcon
-              className="copyright-icon w-[22px] hover:opacity-70 md:w-2"
-              fontSize="small"
-            />
-          </a>
-        </div>
+    <footer className="text-mono-800 dark:text-mono-500 flex h-36 w-full max-w-6xl items-center justify-center self-center px-10 text-[15px] md:fixed md:bottom-0 md:h-12 md:justify-end">
+      <div className="hidden flex-row gap-1 md:flex">
+        <span>Copyright © {new Date().getFullYear()}</span>
+        <a
+          className="underline active:opacity-50 md:hover:opacity-50"
+          target="_blank"
+          href="https://jhuang.ca"
+        >
+          Jarrett Huang
+        </a>
+        <span>|</span>
+        <span>MIT License</span>
+        <span>|</span>
+        <a
+          className="underline active:opacity-50 md:hover:opacity-50"
+          target="_blank"
+          href="https://github.com/jarretthuang/json-viewer"
+        >
+          <GitHubIcon className="h-2 w-2" fontSize="small" />
+        </a>
+        <span>|</span>
+        <a
+          className="active:opacity-50 md:hover:opacity-50"
+          href="https://www.buymeacoffee.com/jarretthuang"
+          target="_blank"
+        >
+          <LocalCafeIcon className="h-2 w-2" fontSize="small" />
+        </a>
       </div>
-    </div>
+      <div className="flex flex-row gap-1 md:hidden">
+        <span>Copyright © {new Date().getFullYear()}</span>
+        <a
+          className="underline active:opacity-50 md:hover:opacity-50"
+          target="_blank"
+          href="https://jhuang.ca"
+        >
+          Jarrett Huang
+        </a>
+      </div>
+    </footer>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,18 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
+        mono: {
+          100: "#f5f5f5", // Slightly off-white
+          200: "#ebebeb", // Very light gray
+          300: "#e1e1e1", // Light gray
+          400: "#cfcfcf", // Medium light gray
+          500: "#b1b1b1", // Neutral gray
+          600: "#9e9e9e", // Slightly darker gray
+          700: "#7e7e7e", // Dark gray
+          800: "#4d4d4d", // Darker gray
+          900: "#2a2a2a", // Almost black
+          1000: "#0f0f0f", // Very close to pure black
+        },
         shakespeare: {
           100: "#4eabd1",
           200: "#267393",
